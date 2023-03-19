2083263f4b8e317d3bc79f4bb07e15d63ec6cfc1
amitdubey <amitdubey6261@gmail.com>
2023-03-20 03:23:47 +0530

    carBody

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -10,19 +10,56 @@ import Enviroment from './Enviroment.js'
 import Experience from '../Experience.js';
 
 export default class World {
-    constructor(){
+    constructor() {
         this.timeStep = 1 / 60;
         this.experience = new Experience();
         this.scene = this.experience.scene;
         this.size = this.experience.sizes;
-        this.resources = this.experience.resources ; 
-        this.createObstacles();
+        this.resources = this.experience.resources;
+        this.createCannonWorld();
     }
 
-    createObstacles(){
+    createCannonWorld() {
+        this.CannonWorld = new CANNON.World({
+            gravity: new CANNON.Vec3(0, -10, 0)
+        });
+
+        this.CannonDebugger = new CannonDebugger(this.scene, this.CannonWorld, {
+            color: 0xff0000,
+            scale: 1.0
+        })
+        this.createGround();
+        this.createObstacles();
+        this.createCar();
+    }
+
+    createGround() {
+        this.ground = new CANNON.Body({
+            shape : new CANNON.Plane(10 , 10),
+            position : (0 , 0 , 0 ),
+            mass : 0 , 
+        })
+
+        this.ground.quaternion.setFromEuler(-Math.PI/2 , 0 , 0 );
+        this.CannonWorld.addBody( this.ground)
+    }
+    createObstacles() {
         this.obstacleArray = new Array(11);
     }
 
-    update(){
+    createCar(){
+        this.CarBody = new CANNON.Body({
+            shape : new CANNON.Box(new CANNON.Vec3( 0.1 , 0.1 , 0.1)),
+            position : new CANNON.Vec3( 0 , 1 , 0 ),
+            mass : 10000
+        })
+
+        this.CannonWorld.addBody(this.CarBody)
+    }
+
+
+    update() {
+        this.CannonWorld.step(this.timeStep);
+        this.CannonDebugger.update()
     }
 }
